b1a16914f634429dbd4d8a142ce6d15d44d0a239
NameHere0 <flip.from.2013@gmail.com>
2026-01-07 18:41:26 +0000

    Added the main operations

--- C++ (LEARNING)/"Project" 1/main.cpp
+++ C++ (LEARNING)/"Project" 1/main.cpp
@@ -4,7 +4,6 @@
     - the program outputs the result of the first and second number with the every operator (i can find)*/
 
 #include "userput.hpp"
-#include "print.hpp"
 #include <iostream>
 
 int main()
@@ -16,9 +15,17 @@ int main()
     // Do the same thing for y
     int y{getUserInput("Enter another integer:")};
 
-    // Now print all solutions including x and y
-    print(x + y);
+    // print x + y and equivelant for others
+    std::cout << "Addition: " << x + y << "\n";       // print the sum
+    std::cout << "Subtraction: " << x - y << "\n";    // print subtraction
+    std::cout << "Multiplication: " << x * y << "\n"; // print product
+    std::cout << "Division: " << x / y << "\n";       // print division
 
-    // cout x + y and newline for every operator
+    // Newline for upcoming
+    std::cout << "\n";
+
+    // Add some extras cuz why not
+    // TODO: FINISH
+
     return 0;
 }

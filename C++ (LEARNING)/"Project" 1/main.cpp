41b9c975f9038eaa007722bcf3618aba950fa01e
NameHere0 <flip.from.2013@gmail.com>
2026-01-07 18:09:31 +0000

    starting a "project (js finishing ch 2)" only "outlined" main (as they said i should)

---
+++ C++ (LEARNING)/"Project" 1/main.cpp
@@ -0,0 +1,16 @@
+/*the classic calculator:
+    - the user inputs a number
+    - the user inputs another number
+    - the program outputs the result of the first and second number with the every operator (i can find)*/
+
+#include <iostream>
+
+int main()
+{
+
+    // Define x as getUserInput()
+    // Do the same thing for y
+
+    // cout x + y and newline for every operator
+    return 0;
+}

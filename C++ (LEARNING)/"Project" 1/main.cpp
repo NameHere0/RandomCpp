1abca05e692a13a3d5d96b2af0d737d0c557a0a3
NameHere0 <flip.from.2013@gmail.com>
2026-01-07 18:21:38 +0000

    Declared x and y on main.cpp

--- C++ (LEARNING)/"Project" 1/main.cpp
+++ C++ (LEARNING)/"Project" 1/main.cpp
@@ -3,13 +3,19 @@
     - the user inputs another number
     - the program outputs the result of the first and second number with the every operator (i can find)*/
 
+#include "userput.hpp"
 #include <iostream>
 
 int main()
 {
 
     // Define x as getUserInput()
+    int x{getUserInput("Enter an integer:")};
+
     // Do the same thing for y
+    int y{getUserInput("Enter another integer:")};
+
+    // Now print all solutions including x and y
 
     // cout x + y and newline for every operator
     return 0;
